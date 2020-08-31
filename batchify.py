9695fe8befab7482c624fe9e48b021a08b8608e8
Simon Renblad <simon.renblad@outlook.com>
2020-08-31 13:37:46 +0800

    added is_batched function

--- batchify.py
+++ batchify.py
@@ -1,5 +1,6 @@
 # made by Simon Renblad
 
+# Converts list into batched_list
 def batchify(target_list, batch_size):
 
     result_list = []
@@ -17,6 +18,7 @@ def batchify(target_list, batch_size):
 
     return result_list
 
+# converts batched_list into list
 def unbatchify(target_list):
 
     result_list = []
@@ -25,3 +27,29 @@ def unbatchify(target_list):
         result_list += batch
 
     return result_list
+
+# boolean function to see whether list is batched_list
+def is_batched(target_list):
+
+    if not isinstance(target_list[0], list):
+        return False
+
+    batch_size = len(target_list[0])
+    num_uniform_batch = len(target_list) - 1
+
+    batch_i = 1
+    while batch_i < num_uniform_batch:
+        if not isinstance(target_list[batch_i], list):
+            return False
+        if len(target_list[batch_i]) != batch_size:
+            return False
+
+        batch_i += 1
+
+    if not isinstance(target_list[num_uniform_batch], list):
+        return False
+
+    if len(target_list[num_uniform_batch]) > batch_size:
+        return False
+
+    return True
